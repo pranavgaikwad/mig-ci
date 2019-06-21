85671f0d089311cd9564584fcf868a6b563827b8
Danil-Grigorev <dgrigore@redhat.com>
2019-06-21 06:34:36 +0200

    Pipeline restructure and removal, OA teardown fix.
    - Pipelines are merged into one, which is triggered by parametrized jobs.
    - Added subscription removal on instance in pre_destroy phase.
    - Builds are started with own CLUSTER_NAME and OCP3_VERSION parametrs from jobs.

--- pipeline/common_stages.groovy
+++ pipeline/common_stages.groovy
@@ -33,6 +33,77 @@ def deployOCP3_OA(prefix = '') {
   }
 }
 
+def deploy_ocp3_agnosticd() {
+  def repo_version = "${OCP3_VERSION.substring(1)}"
+  def releases = [
+    '3.7': "3.7.23",
+    '3.9': "3.9.51",
+    '3.10': "3.10.34",
+    '3.11': "3.11.104"
+  ]
+  def osrelease = releases["${repo_version}"]
+  def envtype = "ocp-workshop"
+  return {
+    stage('Deploy OCP3 cluster with agnosticd') {
+      steps_finished << 'Deploy agnosticd OCP3 workload ' + OCP3_VERSION
+      dir('agnosticd') {
+        withCredentials([
+            string(credentialsId: "$EC2_ACCESS_KEY_ID", variable: 'AWS_ACCESS_KEY_ID'),
+            string(credentialsId: "$EC2_SECRET_ACCESS_KEY", variable: 'AWS_SECRET_ACCESS_KEY'),
+            string(credentialsId: "$EC2_SUB_USER", variable: 'SUB_USER'),
+            string(credentialsId: "$EC2_SUB_PASS", variable: 'SUB_PASS'),
+            string(credentialsId: "$AGND_REPO", variable: 'OWN_REPO')
+            ])
+        {
+          def vars = [
+            "-e 'guid=${GUID}'",
+            "-e 'env_type=${envtype}'",
+            "-e 'own_repo_path=${OWN_REPO}/${osrelease}'",
+            "-e 'osrelease=${osrelease}'",
+            "-e 'repo_version=${repo_version}'",
+            "-e 'cloud_provider=ec2'",
+            "-e 'aws_region=${AWS_REGION}'",
+            "-e 'HostedZoneId=${HOSTZONEID}'",
+            "-e 'key_name=${EC2_KEY}'",
+            "-e 'ansible_ssh_private_key_file=${PRIVATE_KEY}'",
+            "-e 'subdomain_base_suffix=${BASESUFFIX}'",
+            "-e 'node_instance_count=${NODE_COUNT}'",
+            "-e 'software_to_deploy=openshift'",
+            "-e 'redhat_registry_user=${SUB_USER}'",
+            "-e 'redhat_registry_password=${SUB_PASS}'",
+            "-e 'aws_access_key_id=${AWS_ACCESS_KEY_ID}'",
+            "-e 'aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}'",
+            "-e 'email=${EMAIL}' -e'output_dir=${WORKSPACE}'",
+            // User admin with admin password to preserve consistency between different deployment types
+            "-e 'admin_user=admin",
+            "-e 'admin_user_password=admin",
+            // Fix for commit # 8780932 to work
+            "-e 'course_name=ocp-workshop' -e 'platform=aws'",
+            // Workaround for hitting AWS limit of c4.xlarge instances
+            "-e 'bastion_instance_type=t2.large' -e 'master_instance_type=c4.xlarge'",
+            "-e 'infranode_instance_type=c4.4xlarge' -e 'node_instance_type=c4.4xlarge'"
+          ]
+          withEnv(['PATH+EXTRA=~/.local/bin']) {
+            sh "which aws"
+            echo "Region: ${AWS_REGION}"
+            echo "Name: ${GUID}"
+            echo "Version: ${OCP3_VERSION}"
+            ansiColor('xterm') {
+              ansiblePlaybook(
+                playbook: 'ansible/main.yml',
+                extras: "${vars.join(' ')}",
+                hostKeyChecking: false,
+                skippedTags: 'validate_cf_template, generate_env_keys',
+                unbuffered: true,
+                colorized: true)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 def deployOCP4() {
   return {
     stage('Deploy OCP4 cluster') {
@@ -70,6 +141,33 @@ def deploy_NFS() {
           hostKeyChecking: false,
           unbuffered: true,
           colorized: true)
+      }
+    }
+  }
+}
+
+
+def login_agnosticd() {
+  def credentials = [
+    "console_addr=https://master.${GUID}${BASESUFFIX}:443",
+    "user=admin",
+    "passwd=admin"
+  ]
+  return {
+    stage('Login into cluster') {
+      steps_finished << 'Login into cluster ' + OCP3_VERSION
+      withEnv([
+          'PATH+EXTRA=~/bin',
+          "KUBECONFIG=${KUBECONFIG_TMP}"])
+        {
+        ansiColor('xterm') {
+          ansiblePlaybook(
+            playbook: 'login.yml',
+            extras: "${credentials.join(' ')}",
+            hostKeyChecking: false,
+            unbuffered: true,
+            colorized: true)
+        }
       }
     }
   }
@@ -136,6 +234,36 @@ def teardown_OCP3_OA(prefix = '') {
           ansiblePlaybook(
             playbook: 'destroy_ocp3_cluster.yml',
             extras: "${prefix}",
+            hostKeyChecking: false,
+            unbuffered: true,
+            colorized: true)
+        }
+      }
+    }
+  }
+}
+
+def teardown_ocp3_agnosticd() {
+  if (EC2_TERMINATE_INSTANCES) {
+    dir("agnosticd") {
+      withCredentials([
+          string(credentialsId: "$EC2_ACCESS_KEY_ID", variable: 'AWS_ACCESS_KEY_ID'),
+          string(credentialsId: "$EC2_SECRET_ACCESS_KEY", variable: 'AWS_SECRET_ACCESS_KEY'),
+          ])
+      {
+        def teardown_vars = [
+          "-e 'aws_region=${AWS_REGION}'",
+          "-e 'guid=${GUID}'",
+          "-e 'env_type=ocp-workshop'",
+          "-e 'cloud_provider=ec2'",
+          "-e 'aws_access_key_id=${AWS_ACCESS_KEY_ID}'",
+          "-e 'aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}'"
+        ]
+        withEnv(['PATH+EXTRA=~/.local/bin']) {
+          echo "Teardown_vars: ${teardown_vars.join(' ')}"
+          ansiblePlaybook(
+            playbook: "ansible/configs/${ENVTYPE}/destroy_env.yml",
+            extras: "${teardown_vars.join(' ')}",
             hostKeyChecking: false,
             unbuffered: true,
             colorized: true)

5fe71c0a5939f8735b4e5db036010903b6ba3aee
Franco Bladilo <fbladilo@redhat.com>
2019-09-01 09:45:29 -0500

    Disable velero log processing by default
    - Process script command line arguments for operator and velero logs

--- scripts/mig_debug.sh
+++ scripts/mig_debug.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
-# Mig operator logs disabled by default, they are very large
+# Mig operator and velero logs disabled by default (they are very large), enable via script args as needed
 
 OC_BINARY=`which oc`
 MIG_NS="openshift-migration"
 WITH_CONTROLLER="false"
 WITH_OPERATOR_LOGS=${WITH_OPERATOR_LOGS:-false}
+WITH_VELERO_LOGS=${WITH_VELERO_LOGS:-false}
 E2E_NS=${E2E_NS:-"sock-shop robot-shop parks-app mysql-persistent"}
 DATE=`date`
+
+# Process arguments if passed, assume defaults otherwise
+
+function usage () {
+echo
+echo "Valid options are : "
+echo -e "\t-w : Enable velero logs"
+echo -e "\t-o : Enable operator logs"
+echo
+echo "Will continue with defaults..."
+echo
+}
+
+while getopts :woh opt
+do
+    case $opt in
+        w)
+            WITH_VELERO_LOGS=true
+            ;;
+        o)
+            WITH_OPERATOR_LOGS=true
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            ;;
+    esac
+done
 
 echo "############################## BEGIN MIG DEBUG ##################################"
 echo -e "\t\t\t$DATE"
@@ -82,18 +114,21 @@ if [ ${WITH_CONTROLLER} == "true" ]; then
 	${OC_BINARY} -n ${MIG_NS} logs ${mig_controller_pod}
 fi
 
-echo
-echo "====== Velero logs ======"
-echo
-${OC_BINARY} -n ${MIG_NS} logs ${velero_pod}
+if [ ${WITH_VELERO_LOGS} == "true" ]; then
+	echo
+	echo "====== Velero logs ======"
+	echo
+	${OC_BINARY} -n ${MIG_NS} logs ${velero_pod}
+	echo
+fi
 
 if [ ${WITH_OPERATOR_LOGS} == "true" ]; then
-echo
-echo "====== Operator logs ======"
-echo
-${OC_BINARY} -n ${MIG_NS} logs ${operator_pod} -c ansible
-${OC_BINARY} -n ${MIG_NS} logs ${operator_pod} -c operator
-echo
+	echo
+	echo "====== Operator logs ======"
+	echo
+	${OC_BINARY} -n ${MIG_NS} logs ${operator_pod} -c ansible
+	${OC_BINARY} -n ${MIG_NS} logs ${operator_pod} -c operator
+	echo
 fi
 
 # Restic DS spawns multiple pods , collect them first
@@ -106,10 +141,24 @@ for pod in ${restic_pods[@]}; do
 	echo
 	${OC_BINARY} -n ${MIG_NS} logs ${pod}
 done
+
+# Scan openshift-migration for not running pods
+declare -a mig_bad_pods
+echo
+echo "====== ${MIG_NS} check pods not in running state ======"
+echo
+readarray -t mig_bad_pods <<< $(${OC_BINARY} -n ${MIG_NS} get pods --no-headers --field-selector=status.phase!=Running,status.phase!=Succeeded | cut -d " " -f1)
+for pod in ${mig_bad_pods[@]}; do
+	echo
+	echo "====== ${MIG_NS} extract logs for not running pods ======"
+	echo
+	${OC_BINARY} -n ${MIG_NS} logs ${pod}
+done
+
 echo
 echo "##### Process E2E namespaces #####"
 echo
-declare -a bad_pods
+declare -a e2e_bad_pods
 for ns in ${E2E_NS}; do
 	echo
 	echo "====== $ns all ======"
@@ -122,15 +171,15 @@ for ns in ${E2E_NS}; do
 	echo
 	echo "====== $ns check pods not in running state ======"
 	echo
-	readarray -t bad_pods <<< $(${OC_BINARY} -n ${ns} get pods --no-headers --field-selector=status.phase!=Running,status.phase!=Succeeded | cut -d " " -f1)
-	if [ ${#bad_pods[@]} -ne 0 ]; then
+	readarray -t e2e_bad_pods <<< $(${OC_BINARY} -n ${ns} get pods --no-headers --field-selector=status.phase!=Running,status.phase!=Succeeded | cut -d " " -f1)
+	if [ ${#e2e_bad_pods[@]} -ne 0 ]; then
 		echo
 		echo "====== $ns extract logs for not running pods ======"
 		echo
-		for bad_pod in ${bad_pods[@]}; do
-			echo "Processing pod : ${bad_pod}"
-			${OC_BINARY} -n $ns logs ${bad_pod}
+		for pod in ${e2e_bad_pods[@]}; do
+			echo "Processing pod : ${pod}"
+			${OC_BINARY} -n $ns logs ${pod}
 		done
 	fi
-	unset bad_pods
+	unset e2e_bad_pods
 done

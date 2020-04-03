257861d2ed9dcf8554fd6d510c8fd58ef9e849f8
Franco Bladilo <fbladilo@redhat.com>
2020-04-03 16:19:43 -0500

    Install UI by default on destination cluster

--- pipeline/parallel-base.groovy
+++ pipeline/parallel-base.groovy
@@ -59,7 +59,7 @@ booleanParam(defaultValue: true, description: 'Clean up workspace after build', 
 booleanParam(defaultValue: false, description: 'Persistent cluster builds with fixed hostname', name: 'PERSISTENT'),
 booleanParam(defaultValue: false, description: 'Enable debugging', name: 'DEBUG'),
 booleanParam(defaultValue: true, description: 'EC2 terminate instances after build', name: 'EC2_TERMINATE_INSTANCES'),
-booleanParam(defaultValue: false, description: 'Deploy mig controller UI on destination cluster', name: 'MIG_CONTROLLER_UI')])])
+booleanParam(defaultValue: true, description: 'Deploy mig controller UI on destination cluster', name: 'MIG_CONTROLLER_UI')])])
 
 // true/false build parameter that defines if we use OLM to deploy mig operator on OCP4
 USE_OLM = params.USE_OLM

56a3d2a24f9ccf3553c357b7e1f9498d301c024f
Franco Bladilo <fbladilo@redhat.com>
2020-12-21 13:56:03 -0600

    Update agnosticd commit

--- pipeline/utils.groovy
+++ pipeline/utils.groovy
@@ -67,7 +67,7 @@ def prepare_agnosticd() {
   sh 'test -e ~/.local/bin/aws || pip install awscli --upgrade --user'
 
 // Fix checkout to commit before boto removal on agnosticd development branch , see https://github.com/fusor/mig-agnosticd/issues/95
-  checkout([$class: 'GitSCM', branches: [[name: 'development']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'agnosticd']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/redhat-cop/agnosticd.git']]])
+  checkout([$class: 'GitSCM', branches: [[name: 'f9801a0aa9386d0f70290fc295a65ed620f45ab1']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'agnosticd']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/redhat-cop/agnosticd.git']]])
 
   checkout([$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'mig-agnosticd']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/konveyor/mig-agnosticd.git']]])
   // Set agnosticd HOME and add to destroy script
